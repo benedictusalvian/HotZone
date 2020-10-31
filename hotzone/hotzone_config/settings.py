b921b0144dbd597732cf2b3d450599d902f3f51c
benedictusalvian <bene@connect.hku.hk>
2020-10-31 14:32:01 +0800

    Debug set to True

--- hotzone/hotzone_config/settings.py
+++ hotzone/hotzone_config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('HOTZONE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['floating-taiga-48014.herokuapp.com',
                  'localhost', '127.0.0.1']
